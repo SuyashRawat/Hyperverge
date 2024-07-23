05e32af600506ecf1c3003a43e2bef255e8fd31d
Suyash Rawat <rawatsuyash78@gmail.com>
2024-07-23 22:18:39 +0530

    f

--- Frontend/digitalboard/src/App.js
+++ Frontend/digitalboard/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Components/Home"
 import State from './state/State';
-import Home from './Components/Home'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function App() {
   return (
     <>
-    <State>
-          <div className="App">
-        
-          <Routes>
+    {/* <State> */}
+      hello
+      <State>
+        <Router>
+        <Routes>
             {/* <Route exact path="/About" element={<About />} /> */}
-            <Route exact path="/home" element={<Home/>} />
+            <Route exact path="/home" element={<Home />} />
             {/* <Route exact path="/login" element={<Login showAlert={showAlert} />} /> */}
             {/* <Route exact path="/ssignup" element={<Ssignup showAlert={showAlert} />} /> */}
             {/* <Route exact path="/edit-note/:id" element={<Edit showAlert={showAlert} />} /> */}
           </Routes>
-        </div> 
+        </Router>
       </State>
+        {/* <div className="App">
+          <Navbar />
+          {!isCollapsed && <div style={{ marginBottom: '100px' }}></div>}
+          <Alert alert={alert} />
+          <Routes>
+            <Route exact path="/About" element={<About />} />
+            <Route exact path="/" element={<Home showAlert={showAlert} />} />
+            <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+            <Route exact path="/ssignup" element={<Ssignup showAlert={showAlert} />} />
+            <Route exact path="/edit-note/:id" element={<Edit showAlert={showAlert} />} />
+          </Routes>
+        </div> */}
+      {/* </State> */}
     </>
   );
 }
