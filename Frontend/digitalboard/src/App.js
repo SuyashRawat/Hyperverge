23cf8d2642c61e65e13b5c112177e498e305f22d
Suyash Rawat <rawatsuyash78@gmail.com>
2024-07-25 20:47:42 +0530

    f

--- Frontend/digitalboard/src/App.js
+++ Frontend/digitalboard/src/App.js
@@ -13,12 +13,23 @@ import HealthTracker from './Components/HealthTracker';
 function App() {
   return (
     <>
+    <div>
+      <h1>Hyperverge</h1>
+    </div>
+    <div className="container">
+      <div>
+      <PomodoroTimer />
+      
+      </div>
+    </div>
+
+
+
     <div style={{display:'flex'}}>
      <GoogleSlides embedUrl="https://docs.google.com/presentation/d/e/2PACX-1vSHU5lCgMJ3Akb8ovraVAAX4v31zv9WtbDghTsP2Om2iLCHxj4hxwq0oGrIZ4CtgQgUcn7Dbetzxu6l/embed?start=false&loop=false&delayms=3000"/>
 
 
      <PomodoroTimer />
-<<<<<<< HEAD
      </div>
      <div>
       
@@ -26,22 +37,17 @@ function App() {
      </div>
      <ToDoList/>
      <HealthTracker/>
-=======
      <h1>Spotify Player</h1>
      <SpotifyAuth />
->>>>>>> 9fe07ea9dfd2d0ea70c85eb1f245f7ccf7f8be33
     {/* <State> */}
     <Router>
           <div className="App">
          
           <Routes>
             {/* <Route exact path="/About" element={<About />} /> */}
-<<<<<<< HEAD
             {/* <Route exact path="/" element={<Home/>} /> */}
-=======
             <Route exact path="/" element={<Home/>} />
            
->>>>>>> 9fe07ea9dfd2d0ea70c85eb1f245f7ccf7f8be33
             {/* <Route exact path="/login" element={<Login showAlert={showAlert} />} /> */}
             {/* <Route exact path="/ssignup" element={<Ssignup showAlert={showAlert} />} /> */}
             {/* <Route exact path="/edit-note/:id" element={<Edit showAlert={showAlert} />} /> */}
